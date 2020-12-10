ff9a776a6f57a7d23839a3d5eb8cab5191b31e47
railgun569514559 <56210720+railgun569514559@users.noreply.github.com>
2020-12-10 11:32:34 +0800

    Add files via upload

---
+++ dPCR_Gui/my_lib/HoughCircle.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'HoughCircle.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class HoughUI(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(1382, 854)
+        self.gridLayout_2 = QtWidgets.QGridLayout(Form)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.gridLayout = QtWidgets.QGridLayout()
+        self.gridLayout.setObjectName("gridLayout")
+        self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+
