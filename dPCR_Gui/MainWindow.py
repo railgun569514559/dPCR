1ed5ceb0ef91935030b0b6db84ff4a44b2465f13
railgun569514559 <56210720+railgun569514559@users.noreply.github.com>
2020-12-14 23:58:08 +0800

    Add files via upload

--- dPCR_Gui/MainWindow.py
+++ dPCR_Gui/MainWindow.py
@@ -5,22 +5,35 @@ from PyQt5.QtGui import QPixmap, QImage
 from my_lib.dPCR_gui import *
 from my_lib_ever.controler_gui_plus import *
 from my_lib_ever.Image_analysis_gui_plus import ImageAnalysisWidget
+from my_lib_ever.Result_gui_plus import *
 import time
 from PyQt5.QtCore import QTimer, QDateTime
 from PyQt5.QtWidgets import QVBoxLayout, QMainWindow, QApplication, QLabel, QWidget
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 import os
-import cv2
+from my_lib_ever.disheDetect import circleDetect
+import cv2 as cv
 import qdarkstyle
 
-def cv_show(name, img):
-    cv2.namedWindow(name, cv2.WINDOW_NORMAL)  # 设置为WINDOW_NORMAL可以任意缩放
-    cv2.imshow(name, img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
-class MainWindow(Ui_MainWindow,QMainWindow):
+class detectThread(QThread):
+    detectFinish = pyqtSignal()
+
+    def __init__(self, cd):
+        super().__init__()
+        self.cd = cd
+
+    def run(self):
+        img = cv.imread(r'F:\PythonProject\dPCR_Gui\image\FAM_density_mid.tif', 2)
+        start = time.time()
+        self.cd.detect(img)
+        end = time.time()
+        print('time:', end - start)
+        self.detectFinish.emit()
+
+
+class MainWindow(Ui_MainWindow, QMainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
@@ -29,6 +42,20 @@ class MainWindow(Ui_MainWindow,QMainWindow):
         self.ImageAnalysis_gui = ImageAnalysisWidget()
         self.ControllerLayout.addWidget(self.controller_gui)
         self.ImgAnalysisLayout.addWidget(self.ImageAnalysis_gui)
+
+        self.result_gui = ResultGui()
+        self.resultLayout.addWidget(self.result_gui)
+
+        self.initUI()
+
+    def initUI(self):
+        self.cd = circleDetect()
+        self.detectThread = detectThread(self.cd)
+        self.detectCurrentBtn.clicked.connect(self.detectThread.start)
+        self.detectThread.detectFinish.connect(self.detectFinish)
+
+    def detectFinish(self):
+        self.result_gui.setResult([self.cd.color_image,self.cd.bright_dishes,self.cd.dark_dishes,self.cd.bad_dishes])
 
 
 
